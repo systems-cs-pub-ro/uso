25283cb8c4accf00bd51d7ed2b0b6a2155735080
Flavius Anton <f.v.anton@gmail.com>
2014-10-20 01:47:13 +0300

    Lab03: Add file necessary for last bonus exercise

---
+++ lab03/ugly/ugly.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+
+
+
+void f(int a,int b, int c) { printf("I'm an ugly function that prints 3 integers. Here they are: %d %d %d\n", a, b, c);
+}int main()
+{
+    int a=1,b=2,c=3;
+    if(a==1)
+        if(b==2)
+
+
+            if(c==3){f(a,b,c);f(a,b,c);
+                f(a,b,c); f(a,b,c); f(a,b,c);
+                f(a, b, c);f(a, b, c);f(a, b, c);}}
