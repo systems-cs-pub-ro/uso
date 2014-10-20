a7272bae39f2bf06c0bfe882f6bba20b4aeddcb7
Mihai Barbulescu <b12mihai@gmail.com>
2014-10-20 22:02:17 +0300

    fix errors according to text

--- lab03/simple-gcc/hello.c
+++ lab03/simple-gcc/hello.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include<stdio.h>
+#include "utils.h"
 
 int main(void)
 {
+    simple_add(10, 20);
+    do_simple_crap();
     printf("Hello, World");
     return 0;
 }
